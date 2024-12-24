e019dfc74d36feea203b42216f9626eeb8a872c6
davidsuseno <165123725+davidsuseno@users.noreply.github.com>
2024-12-24 13:39:01 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,174 @@
+<?php
+$host = "127.0.0.1";
+$username = "root";
+$password = "";
+$database_name = "booklist";
+$connection = mysqli_connect($host, $username, $password, $database_name);
+
+// === FUNCTION KHUSUS ADMIN START ===
+
+// MENAMPILKAN DATA KATEGORI BUKU
+function queryReadData($dataKategori) {
+  global $connection;
+  $result = mysqli_query($connection, $dataKategori);
+  $items = [];
+  while($item = mysqli_fetch_assoc($result)) {
+    $items[] = $item;
+  }     
+  return $items;
+}
+
+// Menambahkan data buku 
+function tambahBuku($dataBuku) {
+  global $connection;
+  
+  $cover = upload();
+  $idBuku = htmlspecialchars($dataBuku["id_buku"]);
+  $kategoriBuku = $dataBuku["kategori"];
+  $judulBuku = htmlspecialchars($dataBuku["judul"]);
+  $pengarangBuku = htmlspecialchars($dataBuku["pengarang"]);
+  $penerbitBuku = htmlspecialchars($dataBuku["penerbit"]);
+  $tahunTerbit = $dataBuku["tahun_terbit"];
+  $jumlahHalaman = $dataBuku["jumlah_halaman"];
+  $deskripsiBuku = htmlspecialchars($dataBuku["buku_deskripsi"]);
+  
+  if(!$cover) {
+    return 0;
+  } 
+  
+  $queryInsertDataBuku = "INSERT INTO buku VALUES('$cover', '$idBuku', '$kategoriBuku', '$judulBuku', '$pengarangBuku', '$penerbitBuku', '$tahunTerbit', $jumlahHalaman, '$deskripsiBuku')";
+  
+  mysqli_query($connection, $queryInsertDataBuku);
+  return mysqli_affected_rows($connection);
+  
+}       
+
+// Function upload gambar 
+function upload() {
+  $namaFile = $_FILES["cover"]["name"];
+  $ukuranFile = $_FILES["cover"]["size"];
+  $error = $_FILES["cover"]["error"];
+  $tmpName = $_FILES["cover"]["tmp_name"];
+  
+  // cek apakah ada gambar yg diupload
+  if($error === 4) {
+    echo "<script>
+    alert('Silahkan upload cover buku terlebih dahulu!')
+    </script>";
+    return 0;
+  }
+  
+  // cek kesesuaian format gambar
+  $jpg = "jpg";
+  $jpeg = "jpeg";
+  $png = "png";
+  $svg = "svg";
+  $bmp = "bmp";
+  $psd = "psd";
+  $tiff = "tiff";
+  $formatGambarValid = [$jpg, $jpeg, $png, $svg, $bmp, $psd, $tiff];
+  $ekstensiGambar = explode('.', $namaFile);
+  $ekstensiGambar = strtolower(end($ekstensiGambar));
+  
+  if(!in_array($ekstensiGambar, $formatGambarValid)) {
+    echo "<script>
+    alert('Format file tidak sesuai');
+    </script>";
+    return 0;
+  }
+  
+  // batas ukuran file
+  if($ukuranFile > 2000000) {
+    echo "<script>
+    alert('Ukuran file terlalu besar!');
+    </script>";
+    return 0;
+  }
+  
+   //generate nama file baru, agar nama file tdk ada yg sama
+  $namaFileBaru = uniqid();
+  $namaFileBaru .= ".";
+  $namaFileBaru .= $ekstensiGambar;
+  
+  move_uploaded_file($tmpName, '../../imgDB/' . $namaFileBaru);
+  return $namaFileBaru;
+} 
+
+// MENAMPILKAN SESUATU SESUAI DENGAN INPUTAN USER PADA * SEARCH ENGINE *
+function search($keyword) {
+  // search data buku
+  $querySearch = "SELECT * FROM buku 
+  WHERE
+  judul LIKE '%$keyword%' OR
+  kategori LIKE '%$keyword%'
+  ";
+  return queryReadData($querySearch);
+  }
+
+// DELETE DATA Buku
+function delete($bukuId) {
+  global $connection;
+  $queryDeleteBuku = "DELETE FROM buku WHERE id_buku = '$bukuId'
+  ";
+  mysqli_query($connection, $queryDeleteBuku);
+  
+  return mysqli_affected_rows($connection);
+}
+
+// UPDATE || EDIT DATA BUKU 
+function updateBuku($dataBuku) {
+  global $connection;
+
+  $gambarLama = htmlspecialchars($dataBuku["coverLama"]);
+  $idBuku = htmlspecialchars($dataBuku["id_buku"]);
+  $kategoriBuku = $dataBuku["kategori"];
+  $judulBuku = htmlspecialchars($dataBuku["judul"]);
+  $pengarangBuku = htmlspecialchars($dataBuku["pengarang"]);
+  $penerbitBuku = htmlspecialchars($dataBuku["penerbit"]);
+  $tahunTerbit = $dataBuku["tahun_terbit"];
+  $jumlahHalaman = $dataBuku["jumlah_halaman"];
+  $deskripsiBuku = htmlspecialchars($dataBuku["buku_deskripsi"]);
+  
+  
+  // pengecekan mengganti gambar || tidak
+  if($_FILES["cover"]["error"] === 4) {
+    $cover = $gambarLama;
+  }else {
+    $cover = upload();
+  }
+  // 4 === gagal upload gambar
+  // 0 === berhasil upload gambar
+  
+  $queryUpdate = "UPDATE buku SET 
+  cover = '$cover',
+  id_buku = '$idBuku',
+  kategori = '$kategoriBuku',
+  judul = '$judulBuku',
+  pengarang = '$pengarangBuku',
+  penerbit = '$penerbitBuku',
+  tahun_terbit = '$tahunTerbit',
+  jumlah_halaman = $jumlahHalaman,
+  buku_deskripsi = '$deskripsiBuku'
+  WHERE id_buku = '$idBuku'
+  ";
+  
+  mysqli_query($connection, $queryUpdate);
+  return mysqli_affected_rows($connection);
+}
+
+function getAllCategories(){
+  global $connection;
+  $query = "SELECT * FROM kategori_buku";
+  $result = mysqli_query($connection, $query);
+  $items = [];
+  while($item = mysqli_fetch_assoc($result)) {
+    $items[] = $item;
+  }     
+  return $items;
+}
+
+
+// === FUNCTION KHUSUS ADMIN END ===
+?>
+
+
